66953fa390443d55918a5b79d576ee3940e565e6
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-23 03:24:35 +0900

    refactor: add car name error message

---
+++ src/constants/errorConstants.js
@@ -0,0 +1,4 @@
+export const ERROR_MESSAGE = {
+  EMPTY_CAR_NAME: '[ERROR] 자동차 이름이 공백입니다.',
+  INVALID_CAR_NAME_LENGTH: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+}
--- src/utils/handleInputCarName.js
+++ src/utils/handleInputCarName.js
@@ -1,5 +1,6 @@
 import {Console} from "@woowacourse/mission-utils";
 import {INPUT_CAR_NAME} from "../constants/consoleConstants.js";
+import {ERROR_MESSAGE} from "../constants/errorConstants.js";
 
 export async function handleInputCarName () {
   const inputCarNames = await Console.readLineAsync(INPUT_CAR_NAME);
@@ -7,11 +8,11 @@ export async function handleInputCarName () {
 
   carNames.map(car => {
     if (car.trim().length === 0 ) {
-      throw new Error('[Error] 자동차 이름이 공백입니다.');
+      throw new Error(ERROR_MESSAGE.EMPTY_CAR_NAME);
     }
 
     if (car.length > 5) {
-      throw new Error('[Error] 자동차 이름은 5글자를 초과할 수 없습니다.');
+      throw new Error(ERROR_MESSAGE.INVALID_CAR_NAME_LENGTH);
     }
   })
 
