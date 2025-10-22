0e622bfd434d3e2417b9bd6e43d8aba963f2b69c
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-23 02:04:54 +0900

    feat: input car name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import {Console} from "@woowacourse/mission-utils";
+import {handleInputCarName} from "./utils/handleInputCarName.js";
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await handleInputCarName();
+    Console.print(`${carNames}`);
+  }
 }
 
 export default App;
