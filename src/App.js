13b89faf95f51c61fd4a74d4b11e8e224775cf41
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-23 02:09:07 +0900

    feat: add trial count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Console} from "@woowacourse/mission-utils";
 import {handleInputCarName} from "./utils/handleInputCarName.js";
+import {handleTrialCount} from "./utils/handleTrialCount.js";
 
 class App {
   async run() {
     const carNames = await handleInputCarName();
-    Console.print(`${carNames}`);
+    const trialCount = await handleTrialCount();
+    Console.print(`${carNames}, ${trialCount}`);
   }
 }
 
---
+++ src/utils/handleTrialCount.js
@@ -0,0 +1,5 @@
+import {Console} from "@woowacourse/mission-utils";
+
+export async function handleTrialCount() {
+  return await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+}
