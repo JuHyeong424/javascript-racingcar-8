05974e05f07e4cdc416baa86b25ef29ea4413f0e
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-23 03:19:56 +0900

    refactor: handle invalid car name length

--- src/utils/handleInputCarName.js
+++ src/utils/handleInputCarName.js
@@ -2,6 +2,18 @@ import {Console} from "@woowacourse/mission-utils";
 import {INPUT_CAR_NAME} from "../constants/consoleConstants.js";
 
 export async function handleInputCarName () {
-  const carNames = await Console.readLineAsync(INPUT_CAR_NAME);
-  return carNames.split(',');
+  const inputCarNames = await Console.readLineAsync(INPUT_CAR_NAME);
+  const carNames = inputCarNames.split(',');
+
+  carNames.map(car => {
+    if (car.trim().length === 0 ) {
+      throw new Error('[Error] 자동차 이름이 공백입니다.');
+    }
+
+    if (car.length > 5) {
+      throw new Error('[Error] 자동차 이름은 5글자를 초과할 수 없습니다.');
+    }
+  })
+
+  return carNames;
 }
