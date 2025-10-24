e93c478c024a41d13c8b4d9425d93f396fac5f95
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-24 14:32:46 +0900

    refactor: add trial count error message

--- src/constants/errorConstants.js
+++ src/constants/errorConstants.js
@@ -1,4 +1,8 @@
 export const ERROR_MESSAGE = {
   EMPTY_CAR_NAME: '[ERROR] 자동차 이름이 공백입니다.',
   INVALID_CAR_NAME_LENGTH: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+
+  EMPTY_TRIAL_COUNT: '[ERROR] 시도 횟수에 숫자를 작성해 주세요.',
+  ZERO_TRIAL_COUNT: '[ERROR] 시도 횟수가 0 입니다. 1 이상의 정수를 입력해 주세요.',
+  MINUS_TRIAL_COUNT: '[ERROR] 시도 횟수가 음수 입니다. 1 이상의 정수를 입력해 주세요.',
 }
--- src/utils/handleTrialCount.js
+++ src/utils/handleTrialCount.js
@@ -1,6 +1,23 @@
 import {Console} from "@woowacourse/mission-utils";
 import {TRIAL_COUNT} from "../constants/consoleConstants.js";
+import {ERROR_MESSAGE} from "../constants/errorConstants.js";
 
 export async function handleTrialCount() {
-  return await Console.readLineAsync(TRIAL_COUNT);
+  const trialCount = await Console.readLineAsync(TRIAL_COUNT);
+  const numericTrialCount  = Number(trialCount);
+
+  if (isNaN(numericTrialCount )) {
+    throw new Error(ERROR_MESSAGE.EMPTY_TRIAL_COUNT);
+  }
+
+  if (!isNaN(numericTrialCount )) {
+    if (numericTrialCount  === 0) {
+      throw new Error(ERROR_MESSAGE.ZERO_TRIAL_COUNT);
+    }
+    if (!isNaN(numericTrialCount ) && numericTrialCount  < 0) {
+      throw new Error(ERROR_MESSAGE.MINUS_TRIAL_COUNT);
+    }
+  }
+
+  return numericTrialCount ;
 }
