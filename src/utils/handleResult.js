29b00a60b0f30fabd3ed24a0590b0ade793206ed
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-24 16:49:25 +0900

    refactor: reduce indent depth by extracting functions

--- src/utils/handleResult.js
+++ src/utils/handleResult.js
@@ -2,6 +2,22 @@ import {Console} from "@woowacourse/mission-utils";
 import {getRandomNumber} from "./getRandomNumber.js";
 import {EXECUTE_RESULT} from "../constants/consoleConstants.js";
 
+function moveCars(racingCars) {
+  racingCars.forEach(car => {
+    const randomNumber = getRandomNumber();
+    if (randomNumber >= 4) {
+      car.position += 1;
+    }
+  });
+}
+
+function printRoundResult(racingCars) {
+  racingCars.forEach(car => {
+    const dashes = '-'.repeat(car.position);
+    Console.print(`${car.name} : ${dashes}`);
+  });
+}
+
 export function handleResult(carNames, trialCount) {
   const racingCars = carNames.map(name => ({
     name: name,
@@ -11,18 +27,8 @@ export function handleResult(carNames, trialCount) {
   Console.print(EXECUTE_RESULT);
 
   for (let i = 0; i < trialCount; i++) {
-    racingCars.forEach(car => {
-      const randomNumber = getRandomNumber();
-      if (randomNumber >= 4) {
-        car.position += 1;
-      }
-    });
-
-    racingCars.forEach(car => {
-      const dashes = '-'.repeat(car.position);
-      Console.print(`${car.name} : ${dashes}`);
-    });
-
+    moveCars(racingCars);
+    printRoundResult(racingCars);
     Console.print('');
   }
 
